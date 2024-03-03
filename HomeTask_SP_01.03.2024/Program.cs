d2d1a8ab3b807e2e681a16a252e7ac394eaa3859
Aleks0378 <aleksstepan0378@gmail.com>
2024-03-03 21:06:00 +0200

    Добавьте файлы проекта.

---
+++ HomeTask_SP_01.03.2024/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace HomeTask_SP_01._03._2024
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
---
+++ Task_1/Program.cs
@@ -0,0 +1,57 @@
+﻿//Задание 1 Создайте приложение, использующее класс Task.
+//Приложение должно отображать текущее время и дату. Запустите задачу три способами: 
+//■ Через метод Start класса Task; 
+//■ Через метод Task.Factory.StartNew; 
+//■ Через метод Task.Run 
+
+//Задание 2 Напишите асинхронный метод для отображения текущего времени и даты.
+//(async) Метод не должен блокировать основной поток
+
+namespace Task_1
+{
+    internal class Program
+    {
+        class MyClass
+        {
+            public void CurrentTime() => Console.Write($"\n\tCurrent date & time: {DateTime.Now.ToString()}\n");
+
+            public async void CurrentTimeStartTaskAssync()
+            {
+                Console.Write("Method Task.Start is in use: ");
+                Task task = new Task(CurrentTime);
+                task.Start();
+                await task;
+                Console.WriteLine("End of Metod Task.Start");
+            }
+
+            public async void CurrentTimeTaskFactoryAssync()
+            {
+                Console.Write("Method Task.Factory is in use: ");
+                await Task.Factory.StartNew(CurrentTime);
+                Console.WriteLine("End of Metod Task.Factory");
+            }
+
+            public async void CurrentTimeTaskRunAssync()
+            {
+                Console.Write("Method Task.Run is in use: ");
+                await Task.Run(CurrentTime);
+                Console.WriteLine("End of Metod Task.Run");
+            }
+        }
+        
+
+        static void Main(string[] args)
+        {
+           MyClass my = new MyClass();
+            //my.CurrentTime();
+            Console.WriteLine("\n\n");
+            my.CurrentTimeStartTaskAssync();
+            Console.WriteLine("\n\n");
+            my.CurrentTimeTaskFactoryAssync();
+            Console.WriteLine("\n\n");
+            my.CurrentTimeTaskRunAssync();
+            Console.WriteLine("\n\n");
+            Console.ReadKey();
+        }
+    }
+}
